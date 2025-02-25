cf42ae7a1acaedcef664d1eb3e1050bacc80bb61
kimeunsu <kim990824@jbnu.ac.kr>
2025-02-25 14:00:29 +0900

    remove unused lines

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 const express = require("express");
-// const dotenv = require("dotenv");
-// const connectDB = require("./config/db");
 const userRoutes = require("./routes/users");
-
-// dotenv.config();
-// connectDB();
 
 const app = express();
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
 
-module.exports = app;  // 서버 실행을 위한 app 객체 export
+module.exports = app; 
