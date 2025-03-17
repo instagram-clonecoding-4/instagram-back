e05059e8f5e20f5e7d8242b631849657e62827dd
kimeunsu <kim990824@jbnu.ac.kr>
2025-03-17 15:09:30 +0900

    CORS 설정

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ require('dotenv').config();
 const express = require("express");
 const followRoutes = require("./routes/followRoutes");
 const userRoutes = require("./routes/userRoutes");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
-app.listen(7777)
-
+const HOST = "0.0.0.0";
+const PORT = 7777;
+app.listen(PORT, HOST, () => {
+    console.log(`🚀 서버 실행 중: http://${HOST}:${PORT}`);
+  });
 app.use("/follow", followRoutes);
 app.use("/user", userRoutes);
 
