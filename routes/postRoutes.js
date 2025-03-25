961fceccc09729d0ffc19b793c7484287335fd12
kimeunsu <kim990824@jbnu.ac.kr>
2025-03-25 16:57:33 +0900

    라우터 순서에 따른 오류 수정

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -15,9 +15,10 @@ const {
 router.post("/", upload.array("content", 10), createPost);               // 게시물 등록 API
 router.get("/", getPostByUser);                                          // 사용자 별 게시물 조회 API
 router.get("/followedposts", getFollowedPosts);                          // 팔로우한 사용자의 게시물 조회 API
+router.get("/all", getAllpost);                                          // 모든 게시물 조회 API
 router.get("/:post_id", getPost);                                        // 게시물 상세 조회 API
 router.put("/:post_id", upload.array("content", 10), updatePost);        // 게시물 수정 API
 router.delete("/:post_id", deletePost);                                  // 게시물 삭제 API
-router.get("/all", getAllpost);                                          // 모든 게시물 조회 API
+
 
 module.exports = router;
